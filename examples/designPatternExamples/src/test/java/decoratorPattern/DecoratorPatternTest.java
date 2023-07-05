137504986f574a51cebf984fb6892092db3b880c
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-05 16:18:46 +0200

    feat(actions): update yml and create new test

---
+++ examples/designPatternExamples/src/test/java/decoratorPattern/DecoratorPatternTest.java
@@ -0,0 +1,12 @@
+package decoratorPattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DecoratorPatternTest {
+
+    @Test
+    public void testOkForActions() {
+        Assert.assertTrue(true);
+    }
+}
